dfce2efba07703e2b745c9ec4011614c7ac999e3
EvertonAugusto21 <94085866+EvertonAugusto21@users.noreply.github.com>
2022-05-12 20:35:28 -0300

    music_shows
    - inclusao da página show_music

---
+++ lib/pages/show_music/show_music.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ShowMusic extends StatelessWidget {
+  const ShowMusic({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      body:container( ),
+      
+    );
+  }
+}
